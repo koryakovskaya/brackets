2c26210fb79fe52ade4099541462185a1d47a130
koryakovskayafeat: add solutions <koryakovskaya.tatiana@gmail.com>
2020-10-12 15:21:57 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  for (let i = str.length ; i>=0 ; i--) {
+    for (let n = 0; n < bracketsConfig.length; n++){
+      let right = bracketsConfig[n].join('');
+      if (str.includes(right)) {
+        str = str.replace(right,'');
+      }
+    }
+  }
+  return str === "";
 }
